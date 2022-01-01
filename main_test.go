130c7542f23a8ddd9303c24acb886725d52a3dbf
Masato Ikeda <masato.ikeda@gmail.com>
2022-01-02 01:25:04 +0900

    Refactor test code

--- main_test.go
+++ main_test.go
@@ -16,42 +16,47 @@ func init() {
 	}
 }
 
-func TestServerGet(t *testing.T) {
-	conn, err := net.Dial("tcp", "localhost:11211")
-	if err != nil {
-		t.Fatalf("cannot dial host: %v", err)
+func TestServer(t *testing.T) {
+	tests := []struct {
+		name    string
+		inReq   string
+		outResp string
+	}{
+		{
+			"Successful GET",
+			"GET\r\n",
+			"OK\r\n",
+		},
+		{
+			"Non-CRLF GET",
+			"GET",
+			"CLIENT_ERROR malformed request\r\n",
+		},
+		{
+			"Unknown command",
+			"FOO\r\n",
+			"CLIENT_ERROR unknown command\r\n",
+		},
 	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			conn, err := net.Dial("tcp", "localhost:11211")
+			if err != nil {
+				t.Fatalf("cannot dial host: %v", err)
+			}
 
-	fmt.Fprintf(conn, "GET\r\n")
+			fmt.Fprintf(conn, test.inReq)
 
-	resp, err := ioutil.ReadAll(conn)
-	if err != nil {
-		t.Fatalf("cannot read: %v", err)
+			resp, err := ioutil.ReadAll(conn)
+			if err != nil {
+				t.Fatalf("cannot read: %v", err)
+			}
+
+			if string(resp) != test.outResp {
+				t.Fatalf("unexpected response: %v", string(resp))
+			}
+
+			conn.Close()
+		})
 	}
-
-	if string(resp) != "OK\r\n" {
-		t.Fatalf("unexpected response: %v", string(resp))
-	}
-
-	conn.Close()
-}
-
-func TestServerUnknownCommand(t *testing.T) {
-	conn, err := net.Dial("tcp", "localhost:11211")
-	if err != nil {
-		t.Fatalf("cannot dial host: %v", err)
-	}
-
-	fmt.Fprintf(conn, "FOO\r\n")
-
-	resp, err := ioutil.ReadAll(conn)
-	if err != nil {
-		t.Fatalf("cannot read: %v", err)
-	}
-
-	if string(resp) != "CLIENT_ERROR unknown command\r\n" {
-		t.Fatalf("unexpected response: %v", string(resp))
-	}
-
-	conn.Close()
 }
